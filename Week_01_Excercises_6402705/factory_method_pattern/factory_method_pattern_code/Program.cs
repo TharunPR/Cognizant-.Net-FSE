aba84434fd2a7c0e968f8d466314afba657c1608
Tharun <tharun.r.2468@gmail.com>
2025-06-22 23:10:37 +0530

    Add files via upload

---
+++ Week_01_Excercises_6402705/factory_method_pattern/factory_method_pattern_code/Program.cs
@@ -0,0 +1,53 @@
+﻿public abstract class Product
+{
+    public abstract string GetDetails();
+}
+
+public class Book : Product
+{
+    public override string GetDetails() => "[Product] This is a BOOK.";
+}
+
+public class Laptop : Product
+{
+    public override string GetDetails() => "[Product] This is a LAPTOP.";
+}
+
+public abstract class ProductFactory
+{
+    public abstract Product CreateProduct();
+}
+
+public class BookFactory : ProductFactory
+{
+    public override Product CreateProduct()
+    {
+        Console.WriteLine("[Factory] Creating a Book...");
+        return new Book();
+    }
+}
+
+public class LaptopFactory : ProductFactory
+{
+    public override Product CreateProduct()
+    {
+        Console.WriteLine("[Factory] Creating a Laptop...");
+        return new Laptop();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("--- Factory Method Pattern ---");
+
+        ProductFactory bookFactory = new BookFactory();
+        Product book = bookFactory.CreateProduct();
+        Console.WriteLine(book.GetDetails());
+
+        ProductFactory laptopFactory = new LaptopFactory();
+        Product laptop = laptopFactory.CreateProduct();
+        Console.WriteLine(laptop.GetDetails());
+    }
+}
